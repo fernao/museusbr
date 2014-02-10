ab0293a184465c49565e89d2be11043501762314
Fernão Lopes <fernao@riseup.net>
2014-02-09 22:35:40 -0200

    fechando museu quando abre outro (sem transicoes)

--- static/js/modules/museu/IndexView.js
+++ static/js/modules/museu/IndexView.js
@@ -25,7 +25,7 @@ define([
 		var nid = match[1];
 		
 		// toggle q abre/fecha museu
-		_toggle_home_div(target);
+		_toggle_home_div(target, nid);
 		
 		// inicializa museu internamente	
 		_init_museu(nid);
@@ -35,7 +35,6 @@ define([
 	    // - carrega varias informacoes do museu quando ele é clicado
 	    // - inicia navegacao no museu particular
 	    _init_museu = function(nid) {
-		console.log("buscando museu " + nid);
 		
 		// carrega vazio primeiro
 		_load_museu_home(nid);
@@ -46,7 +45,6 @@ define([
 			dataMuseu = {
 			    museu: museu.attributes[0]
 			}
-			console.log(dataMuseu);
 			_load_museu_home(nid, dataMuseu);
 		    }
 		});
@@ -67,35 +65,55 @@ define([
 		}
 	    }
 	    
-	    _toggle_home_div = function(el) {
-		$(el).css('height', '200');
+	    _toggle_home_div = function(el, nid) {
+
+		if ($('body').data('museu_ativo')) {
+		    museu_ativo = $('body').data('museu_ativo');
+		    el_fechar = museu_ativo  + " .museu-subpages";
+		    
+		    // restaura estado do museu ativo
+		    // TODO: colocar transicoes
+		    $(museu_ativo).css('height', 97); // TODO: puxar das settings 
+		    $(el_fechar).remove();
+		}
+		
+		// define novo museu aberto
+		$('body').data('museu_ativo', '#nid_' + nid);
+		
+		// TODO: colocar transicoes
+		$(el).css('height', '200'); // TODO: puxar das settings
 	    }
 	    
 	    //// _toggle_navigation
 	    // - coloca/tira setas 
 	    _toggle_navigation = function(nid) {
 		el = "#nid_" + nid;
+
 		$(el).prepend(MuseuNavigationTpl);
 		$(el + " #left").click(function(e) { _navigate(nid, 'left') });
 		$(el + " #right").click(function(e) { _navigate(nid, 'right') });
 	    }
 	    
 	    _navigate = function(nid, direction) {
+		// pegar o estado atual?
+		
 		console.log(direction);
 		// gerencia setas
 	    }
 	    
 	    // carrega museus - tela inicial
 	    _load_museus = function() {
-	
+		// armazena museu ativo
+		$('body').data('museu_ativo', false);
+		
 		var museus = new MuseuCollection([]);
 		museus.fetch({
 		    success: function() {
 			nodes = museus.models[0].attributes;
-			console.log(nodes)
 			data = {
 			    nodes: nodes
 			}
+			
 			var compiledTemplate = _.template(MuseuIndexTpl, data);
 			$('#content').html(compiledTemplate);
 			$('div.museu-row').click(function(e) { toggle_museu(e) });
