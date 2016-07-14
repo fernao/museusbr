1c8b55f34bba0a20edcc8f4c388443b3632a56c9
fernao <fernao@riseup.net>
2016-07-14 13:46:41 -0300

    ajustes no mapa - ainda não funcional

--- js/modules/museu/IndexView.js
+++ js/modules/museu/IndexView.js
@@ -105,7 +105,9 @@ define([
 				    }	
 				    
 				    var compiledMapa = _.template(MapaTpl, data);
-				    $('#mapa-posicao').html(compiledMapa);	    
+				    $('#mapa-posicao').html(compiledMapa);
+				    _resize_map();
+				    
 				    var compiledRegiao = _.template(RegiaoTpl, data);
 				    $('#lista-cidades').html(compiledRegiao);
 				}
@@ -126,7 +128,6 @@ define([
 				}
 				
 				var compiledTags = _.template(TagsTpl, data);
-				console.log(compiledTags);
 				$('#tag_cloud').html(compiledTags);
 				
 				$.fn.tagcloud.defaults = {
@@ -151,7 +152,9 @@ define([
 			    }
 			    
 			    var compiledMapa = _.template(MapaTpl, data);
-			    $('#mapa-posicao').html(compiledMapa);	    
+			    $('#mapa-posicao').html(compiledMapa);
+			    _resize_map();
+			    
 			    var compiledRegiao = _.template(RegiaoTpl, data);
 			    $('#lista-cidades').html(compiledRegiao);	    
 			}
@@ -441,6 +444,37 @@ define([
 		}
 	    }
 
+	    _resize_map = function() {
+		window.onload = function () {
+		    var ImageMap = function (map) {
+			var n,
+			    areas = map.getElementsByTagName('area'),
+			    len = areas.length,
+			    coords = [],
+			    previousWidth = 1920;
+			for (n = 0; n < len; n++) {
+			    coords[n] = areas[n].coords.split(',');
+			}
+			this.resize = function () {
+			    var n, m, clen,
+				x = document.body.clientWidth / previousWidth;
+			    for (n = 0; n < len; n++) {
+				clen = coords[n].length;
+				for (m = 0; m < clen; m++) {
+				    coords[n][m] *= x;
+				}
+				areas[n].coords = coords[n].join(',');
+			    }
+			    previousWidth = document.body.clientWidth;
+			    return true;
+			};
+			window.onresize = this.resize;
+		    },
+			imageMap = new ImageMap(document.getElementById('map_ID'));
+		    imageMap.resize();
+		}
+	    }
+	    
 	    // init main functionalities
 	    _init_main = function(lang, tags, localizacao) {
 		var tags = tags || 'todos',
